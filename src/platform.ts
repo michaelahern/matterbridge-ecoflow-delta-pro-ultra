a0feb773ac974ac1831950be5f28d39c800b4c38
Michael Ahern <michael.ahern@gmail.com>
2025-06-09 03:15:56 +0000

    More Integration!

--- src/platform.ts
+++ src/platform.ts
@@ -1,8 +1,11 @@
 import { RestClient } from '@ecoflow-api/rest-client';
-import { Matterbridge, MatterbridgeEndpoint, MatterbridgeDynamicPlatform, PlatformConfig } from 'matterbridge';
+import { Matterbridge, MatterbridgeEndpoint, MatterbridgeDynamicPlatform, PlatformConfig, batteryStorage, bridgedNode, electricalSensor, powerSource } from 'matterbridge';
+import { PowerSource } from 'matterbridge/matter/clusters';
 import { AnsiLogger } from 'matterbridge/logger';
+import { PowerSourceTag } from 'matterbridge/matter';
 import mqtt from 'mqtt';
-import { mqttResponseBaseSchema, mqttResponseCmdId1Params } from './schemas.js';
+
+import { mqttResponseBaseSchema, mqttResponseCmdId1Params, mqttResponseCmdId2Params, mqttResponseCmdId3Params, restAllQuotaData } from './schemas.js';
 
 export class EcoflowDeltaProUltraPlatform extends MatterbridgeDynamicPlatform {
     ecoflowRestClient: RestClient;
@@ -36,44 +39,98 @@ export class EcoflowDeltaProUltraPlatform extends MatterbridgeDynamicPlatform {
 
         await this.ready;
         await this.clearSelect();
+
+        const devices = await this.ecoflowRestClient.getDevicesPlain();
+
+        for (const device of devices.data) {
+            if (device.productName !== 'DELTA Pro Ultra') {
+                continue;
+            }
+
+            const devicePropsResponse = await this.ecoflowRestClient.getDevicePropertiesPlain(devices.data[0].sn);
+            const deviceProps = restAllQuotaData.parse(devicePropsResponse.data);
+
+            this.log.info('Properties:', deviceProps);
+
+            const batteryLevel = deviceProps['hs_yj751_pd_appshow_addr.soc'];
+            const voltsIn = deviceProps['hs_yj751_pd_backend_addr.inAcC20Vol'];
+            const ampsIn = deviceProps['hs_yj751_pd_backend_addr.inAcC20Amp'];
+            const wattsIn = deviceProps['hs_yj751_pd_appshow_addr.wattsInSum'];
+            const acInFreq = deviceProps['hs_yj751_pd_backend_addr.acInFreq'];
+
+            const endpoint = new MatterbridgeEndpoint([batteryStorage, bridgedNode], { uniqueStorageKey: 'EcoFlow-' + device.sn }, this.config.debug as boolean)
+                .createDefaultBridgedDeviceBasicInformationClusterServer(
+                    device.deviceName ?? 'DELTA Pro Ultra',
+                    device.sn,
+                    0xfff1,
+                    'EcoFlow',
+                    device.productName,
+                    parseInt(this.version.replace(/\D/g, '')),
+                    this.version,
+                    parseInt(this.matterbridge.matterbridgeVersion.replace(/\D/g, '')),
+                    this.matterbridge.matterbridgeVersion)
+                .addRequiredClusterServers();
+
+            endpoint.addChildDeviceType('Battery', powerSource, { tagList: [PowerSourceTag.Battery] })
+                .createDefaultPowerSourceRechargeableBatteryClusterServer(batteryLevel, PowerSource.BatChargeLevel.Ok, 102.4 * 1000)
+                .addRequiredClusterServers();
+
+            endpoint.addChildDeviceType('Grid', powerSource, { tagList: [PowerSourceTag.Grid] })
+                .createDefaultPowerSourceWiredClusterServer(PowerSource.WiredCurrentType.Ac)
+                .addRequiredClusterServers();
+
+            endpoint.addChildDeviceType('FromGrid', electricalSensor)
+                .createDefaultElectricalEnergyMeasurementClusterServer()
+                .createDefaultElectricalPowerMeasurementClusterServer(voltsIn ? voltsIn * 1000 : null, ampsIn ? ampsIn * 1000 : null, wattsIn ? wattsIn * 1000 : null, acInFreq ? acInFreq * 1000 : null)
+                .createDefaultPowerTopologyClusterServer()
+                .addRequiredClusterServers();
+
+            // endpoint.addChildDeviceType('ToOutlets', electricalSensor)
+            //     .createDefaultElectricalEnergyMeasurementClusterServer()
+            //     .createDefaultElectricalPowerMeasurementClusterServer(null, null, wattsOut * 1000, acOutFreq * 1000)
+            //     .createDefaultPowerTopologyClusterServer()
+            //     .addRequiredClusterServers();
+
+            this.setSelectDevice(device.sn, device.deviceName ?? 'DELTA Pro Ultra', undefined, 'hub');
+            await this.registerDevice(endpoint);
+            this.bridgedDevices.set(device.sn, endpoint);
+        }
     }
 
     override async onConfigure() {
         await super.onConfigure();
         this.log.info('[onConfigure]');
 
-        const devices = await this.ecoflowRestClient.getDevicesPlain();
-        console.log(devices);
-
-        const deviceProperties = await this.ecoflowRestClient.getDevicePropertiesPlain(devices.data[0].sn);
-        console.log(deviceProperties);
-
         const mqttCreds = await this.ecoflowRestClient.getMqttCredentials();
-        console.log(mqttCreds);
-
         const mqttClient = await mqtt.connectAsync(`${mqttCreds.protocol}://${mqttCreds.url}:${mqttCreds.port}`, {
             username: `${mqttCreds.certificateAccount}`,
             password: `${mqttCreds.certificatePassword}`,
             protocolVersion: 5
         });
 
-        await mqttClient.subscribeAsync(`/open/${mqttCreds.certificateAccount}/${devices.data[0].sn}/quota`);
-        await mqttClient.subscribeAsync(`/open/${mqttCreds.certificateAccount}/${devices.data[0].sn}/status`);
+        for (const deviceSerialNumber of this.bridgedDevices.keys()) {
+            await mqttClient.subscribeAsync(`/open/${mqttCreds.certificateAccount}/${deviceSerialNumber}/quota`);
+            await mqttClient.subscribeAsync(`/open/${mqttCreds.certificateAccount}/${deviceSerialNumber}/status`);
+        }
 
         mqttClient.on('message', (topic, message) => {
-            console.log('Topic:', topic);
             const messageWrapper = mqttResponseBaseSchema.parse(JSON.parse(message.toString()));
-
             switch (messageWrapper.cmdId) {
                 case 1: {
                     const params = mqttResponseCmdId1Params.parse(messageWrapper.param);
-                    console.log('cmdId 1:', messageWrapper, params);
+                    this.log.info('MQTT Message', topic, messageWrapper.cmdId, messageWrapper.cmdFunc, params);
                     break;
                 }
-                case 28:
+                case 2: {
+                    const params = mqttResponseCmdId2Params.parse(messageWrapper.param);
+                    this.log.info('MQTT Message', topic, messageWrapper.cmdId, messageWrapper.cmdFunc, params);
                     break;
-                default:
-                    console.log('cmd other', messageWrapper);
+                }
+                case 3: {
+                    const params = mqttResponseCmdId3Params.parse(messageWrapper.param);
+                    this.log.info('MQTT Message', topic, messageWrapper.cmdId, messageWrapper.cmdFunc, params);
+                    break;
+                }
             }
         });
     }
