3a4c6729cee1322d37746acbbe9a590467369ecd
Michael Ahern <michael.ahern@gmail.com>
2025-10-04 19:13:19 +0000

    Matterbridge 3.3.0 Plugin Isolation

--- src/module.ts
+++ src/module.ts
@@ -1,7 +1,7 @@
-import { Matterbridge, type PlatformConfig } from 'matterbridge';
+import { type PlatformConfig, type PlatformMatterbridge } from 'matterbridge';
 import { AnsiLogger } from 'matterbridge/logger';
 import { EcoflowDeltaProUltraPlatform } from './platform.js';
 
-export default function initializePlugin(matterbridge: Matterbridge, log: AnsiLogger, config: PlatformConfig): EcoflowDeltaProUltraPlatform {
+export default function initializePlugin(matterbridge: PlatformMatterbridge, log: AnsiLogger, config: PlatformConfig): EcoflowDeltaProUltraPlatform {
     return new EcoflowDeltaProUltraPlatform(matterbridge, log, config);
 }
